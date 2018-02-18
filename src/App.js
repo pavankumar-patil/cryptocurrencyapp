3739afb74168fa384179333ff38babe517a231a8
pavan <patil.pavan904@gmail.com>
2018-02-18 19:56:04 +0530

    new file added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import Tickers from './component/Tickers.js';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    componentWillMount() {
+      console.log('Component WILL MOUNT!')
+   }
+   componentDidMount() {
+      console.log('Component DID MOUNT!')
+   }
+   componentWillReceiveProps(newProps) {    
+      console.log('Component WILL RECIEVE PROPS!')
+   }
+   shouldComponentUpdate(newProps, newState) {
+      return true;
+   }
+   componentWillUpdate(nextProps, nextState) {
+      console.log('Component WILL UPDATE!');
+   }
+   componentDidUpdate(prevProps, prevState) {
+      console.log('Component DID UPDATE!')
+   }
+   componentWillUnmount() {
+      console.log('Component WILL UNMOUNT!')
+   }
   render() {
     return (
       <div className="App">
       <div className="App-header">
       <h2>mytestapp</h2>
       </div>
-      </div>,<Tickers/>
+      <Tickers/>
+      </div>
     );
   }
 }
--- src/component/Tickers.js
+++ src/component/Tickers.js
@@ -1,34 +1,60 @@
  import React, { Component } from 'react';
-    import './Tickers.css';
-
+    import './Tickers.css';import axios from 'axios';
+import  Cryptocurrency from './Cryptocurrency.js'
     class Tickers extends Component {
 
         constructor(props) {
             super(props);
             this.state = {
-                data: [
-                    {
-                        id: "bitcoin",
-                        price: "1"
-                    },
-                    {
-                        id: "ethereum",
-                        price: "1"
-                    },
-                    {
-                        id: "litecoin",
-                        price: "1"
-                    }
-                ]
-            };
+        data: [
+            {
+                id: "bitcoin",
+                name: "Bitcoin",
+                symbol: "BTC",
+                price_usd: "1",
+                percent_change_1h: "0",
+                percent_change_24h: "0",
+                percent_change_7d: "0",
+            },
+            {
+                id: "ethereum",
+                name: "Ethereum",
+                symbol: "ETH",
+                price_usd: "1",
+                percent_change_1h: "0",
+                percent_change_24h: "0",
+                percent_change_7d: "0",
+            },
+            {
+                id: "litecoin",
+                name: "Litecoin",
+                symbol: "LTC",
+                price_usd: "1",
+                percent_change_1h: "0",
+                percent_change_24h: "0",
+                percent_change_7d: "0",
+            }
+        ]
+    };
+        }
+  componentDidMount() {
+            this.fetchCryptocurrencyData();
+            this.interval = setInterval(() => this.fetchCryptocurrencyData(), 10 * 1000);
         }
 
+ fetchCryptocurrencyData() {
+       fetch("https://api.coinmarketcap.com/v1/ticker/?limit=10")
+            .then(response => {
+                console.log("response"+JSON.stringify(response));
+                var wanted = ["bitcoin", "ethereum", "litecoin"];
+                var result = response.data.filter(currency => wanted.includes(currency.id));
+                this.setState({ data: result});
+            })
+            .catch(err => console.log(err));
+    }
         render() {
             var tickers = this.state.data.map((currency) =>
-                <li key={currency.id}>
-                    <h3>{currency.id}</h3>
-                    <h4>{currency.price} USD</h4>
-                </li>
+                <Cryptocurrency data={currency} key={currency.id}/>
             );
             return (
                 <div className="tickers-container">
